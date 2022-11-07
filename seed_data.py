9a6c62b633b5d0c17277231f4fc2ac76e0a9d60a
Joshua Roig <joshua.roig@hotmail.com>
2022-11-07 20:29:59 -0300

    Se modificó el archivo seed_data.py

--- seed_data.py
+++ seed_data.py
@@ -11,6 +11,6 @@ print("Se cargo con éxito los Familiares de pruebas")
 
 from blog.models import Post
 
-Post(title="Mi post CODEHOUSE", short_content="un post de CODEHOUSE", content="sadljalsjdlkajsdljasljdlaksjd").save()
+Post(title="Post de PRUEBA", short_content="Espacio destinado a un texto descriptivo corto", content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.").save()
 
 print("Se cargo con éxito los post de pruebas")
