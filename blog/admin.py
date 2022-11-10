1b5581ff2587917dab0344ec44a4e1ff1c7ffeba
Leandro Bouza <averno.leo@gmail.com>
2022-11-10 14:27:54 -0300

    Se verifica que ya crea un Usuario, aparece en el admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
 from blog.models import Post # agregado para gestionar desde el Admin
+from blog.models import Usuario
 
 admin.site.register(Post) # agregado para gestionar desde el Admin
+admin.site.register(Usuario)
